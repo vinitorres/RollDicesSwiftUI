6643c527370499e598e83c0e3a57182492911d06
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2022-06-20 20:42:30 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Andre Vinicius Torres Conrado on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
