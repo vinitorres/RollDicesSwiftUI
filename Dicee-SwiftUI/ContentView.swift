bc9b828371504d800b578800e83b2a705e01ba7c
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2022-06-21 07:00:40 -0300

    finished example

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    func rollDices() {
+        leftDiceNumber = Int.random(in: 1...6)
+        rightDiceNumber = Int.random(in: 1...6)
+    }
+
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background").resizable().ignoresSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    rollDices()
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1.0, contentMode: .fit)
             .padding()
     }
 }
@@ -17,5 +59,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
